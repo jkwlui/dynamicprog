331ac85d027a5e6c80e2b341cea1e6488c562aac
Jonathan Lui <kinwa91@msn.com>
2015-03-21 21:09:33 -0700

    implemented import words from file.

--- SlackSpace/src/model/Main.java
+++ SlackSpace/src/model/Main.java
@@ -1,14 +1,13 @@
 package model;
-
 public class Main {
 
 	
 	public static void main(String[] args) {
+		// get words from file:
+		String[] words3 = Utilities.getWordsFromFile("words.txt");
 		
-		String[] words = {"A", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"};
-		String[] words2 = {"A", "green", "cat"};
-		int maxCharInLine = 10;
-		int solution = Naive.minSlackSpace(words2, maxCharInLine);
+		int maxCharInLine = 5;
+		int solution = Naive.minSlackSpace(words3, maxCharInLine);
 		System.out.println(solution);
 	}
 
--- SlackSpace/src/model/Utilities.java
+++ SlackSpace/src/model/Utilities.java
@@ -1,5 +1,15 @@
 package model;
 
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
 public class Utilities {
 	
 	public static int charsInLine(String[] words) {
@@ -10,6 +20,25 @@ public class Utilities {
 		}
 		numChars--;  // minus 1 space at the end of line
 		return numChars;
+	}
+	
+	public static String[] getWordsFromFile(String filename) {
+		List<String> lines = new ArrayList<String>();
+		try {
+			FileReader fileReader = new FileReader(filename);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
+				lines.add(line);
+			}
+			bufferedReader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lines.toArray(new String[lines.size()]);
 	}
 	
 
